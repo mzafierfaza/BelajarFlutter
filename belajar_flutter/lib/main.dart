eddb45e3d7513743fa7fead2f6cf5fe76d764e3b
mzafierfaza <zafierfazamuhammad@gmail.com>
2021-02-23 16:47:18 +0700

    add custom_card

--- belajar_flutter/lib/main.dart
+++ belajar_flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:belajar_flutter/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,74 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home:
-          MyPage(), // harus kayak gini kalo mau pake MediaQuery, kalo gak dia akan error
+          MainPage(), // harus kayak gini kalo mau pake MediaQuery, kalo gak dia akan error
     );
-  }
-}
-
-class MyPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Media Query"),
-      ),
-      body: Row(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                child: Text("Ini adalah Container dengan Media Querry"),
-                color: Colors.red,
-                // media querry akan otomatis fit dibagi dari layar android,
-                width: MediaQuery.of(context).size.width / 3,
-                height: MediaQuery.of(context).size.height / 4,
-              ),
-              Container(
-                child: Text(
-                    "Height dan width nya otomatis dibagi dari layar hp masing2"),
-                color: Colors.blue,
-                width: MediaQuery.of(context).size.width / 3,
-                height: MediaQuery.of(context).size.height / 4,
-              ),
-            ],
-          ),
-          (MediaQuery.of(context).orientation == Orientation.portrait)
-              // ini pakai if singkat. (if) ? benar : salah
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: generateContainer(),
-                )
-              : Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: generateContainer(),
-                )
-        ],
-      ),
-    );
-  }
-
-  List<Widget> generateContainer() {
-    return [
-      Container(
-        child: Text("ini otomatis jika layar orientasi layar berubah"),
-        width: 80,
-        height: 80,
-        color: Colors.amber,
-      ),
-      Container(
-        child: Text("maka media query akan otomatis mengubah juga "),
-        width: 80,
-        height: 80,
-        color: Colors.green,
-      ),
-      Container(
-        child: Text("sesuai dengan yang ditentukan"),
-        width: 80,
-        height: 80,
-        color: Colors.purple,
-      )
-    ];
   }
 }
