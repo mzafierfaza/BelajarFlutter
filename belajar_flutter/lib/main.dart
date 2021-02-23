8449de5cc9e6f45a043b7286fb58331b4e6767d7
mzafierfaza <zafierfazamuhammad@gmail.com>
2021-02-23 14:58:59 +0700

    add MediaQuerry

--- belajar_flutter/lib/main.dart
+++ belajar_flutter/lib/main.dart
@@ -4,38 +4,79 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("TextField"),
-      ),
-      body: Container(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextField(
-                // obscureText: true, //  ini buat password
-                // maxLength: 4, // panjang karakter
+      home:
+          MyPage(), // harus kayak gini kalo mau pake MediaQuery, kalo gak dia akan error
+    );
+  }
+}
 
-                onChanged: (value) {
-                  setState(() {});
-                },
-                controller: controller,
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Media Query"),
+      ),
+      body: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                child: Text("Ini adalah Container dengan Media Querry"),
+                color: Colors.red,
+                // media querry akan otomatis fit dibagi dari layar android,
+                width: MediaQuery.of(context).size.width / 3,
+                height: MediaQuery.of(context).size.height / 4,
               ),
-              Text(controller.text),
+              Container(
+                child: Text(
+                    "Height dan width nya otomatis dibagi dari layar hp masing2"),
+                color: Colors.blue,
+                width: MediaQuery.of(context).size.width / 3,
+                height: MediaQuery.of(context).size.height / 4,
+              ),
             ],
-          )),
-    ));
+          ),
+          (MediaQuery.of(context).orientation == Orientation.portrait)
+              // ini pakai if singkat. (if) ? benar : salah
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: generateContainer(),
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: generateContainer(),
+                )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> generateContainer() {
+    return [
+      Container(
+        child: Text("ini otomatis jika layar orientasi layar berubah"),
+        width: 80,
+        height: 80,
+        color: Colors.amber,
+      ),
+      Container(
+        child: Text("maka media query akan otomatis mengubah juga "),
+        width: 80,
+        height: 80,
+        color: Colors.green,
+      ),
+      Container(
+        child: Text("sesuai dengan yang ditentukan"),
+        width: 80,
+        height: 80,
+        color: Colors.purple,
+      )
+    ];
   }
 }
