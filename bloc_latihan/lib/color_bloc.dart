0dd800e2c4f8d0e33ea7c2ffe08ab9b2472a9d78
mzafierfaza <zafierfazamuhammad@gmail.com>
2021-02-25 00:18:02 +0700

    add bloc with library

--- bloc_latihan/lib/color_bloc.dart
+++ bloc_latihan/lib/color_bloc.dart
@@ -1,45 +1,22 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
 
-enum ColorEvent {
-  to_amber,
-  to_light_blue
-} // enum untuk membuat jenis-jenis color event
+enum ColorEvent { to_amber, to_light_blue }
 
-class ColorBloc {
-  Color _color = Colors.amber; // color pertama x amber
-  // _color private gakbisa di akses dari luar
-  StreamController<ColorEvent> _eventController = // event = Output
-      StreamController<ColorEvent>();
-  StreamSink<ColorEvent> get eventSink => _eventController.sink;
+class ColorBloc extends Bloc<ColorEvent, Color> {
+  Color _color = Colors.amber;
 
-  StreamController<Color> _stateController =
-      StreamController<Color>(); // state = Input
-  StreamSink<Color> get _stateSink => _stateController.sink;
+  @override
+  Color get initialState => Colors.amber;
 
-  Stream<Color> get stateStream => _stateController.stream;
-  // ketika UI akan memberikan event masuk lewat sink, maka stream akan masuk ke mapEvent,
-  // berdasarkan color event dia akan mengubah state.
+  // ColorBloc(Color initialState) : super(initialState);
 
-  // berfungsi sebagai prosesor.
-  void _mapEventToState(ColorEvent colorEvent) {
-    if (colorEvent == ColorEvent.to_amber)
-      _color = Colors.amber;
-    else
-      _color = Colors.lightBlue;
+  @override
+  Stream<Color> mapEventToState(ColorEvent event) async* {
+    _color = (event == ColorEvent.to_amber) ? Colors.amber : Colors.lightBlue;
 
-    _stateSink.add(_color);
-  }
+    yield _color;
 
-  // untuk menghubungkan abis dari streamcontroller lanjut ke _mapEventState
-  ColorBloc() {
-    _eventController.stream.listen(_mapEventToState);
-  }
-
-  void dispose() {
-    // dispose untuk langsung menutup streamcontroller biar tidak makan memori
-    _eventController.close();
-    _stateController.close();
+    // throw UnimplementedError();
   }
 }
--- bloc_latihan/lib/main.dart
+++ bloc_latihan/lib/main.dart
@@ -1,60 +1,54 @@
 import 'package:bloc_latihan/color_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ColorBloc bloc = ColorBloc();
-
-  @override
-  void dispose() {
-    bloc.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("BLoC Tanpa Library"),
-            ),
-            floatingActionButton: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    bloc.eventSink.add(ColorEvent.to_amber);
-                  },
-                  backgroundColor: Colors.amber,
-                ),
-                SizedBox(width: 10),
-                FloatingActionButton(
-                    onPressed: () {
-                      bloc.eventSink.add(ColorEvent.to_light_blue);
-                    },
-                    backgroundColor: Colors.lightBlue)
-              ],
-            ),
-            body: Center(
-                child: StreamBuilder(
-              // untuk membuild widget setiap kali dia dapatkan dari stream
-              stream: bloc.stateStream,
-              initialData: Colors.amber,
-              builder: (context, snapshot) {
-                return AnimatedContainer(
-                  duration: Duration(milliseconds: 500),
-                  width: 100,
-                  height: 100,
-                  color: snapshot.data,
-                );
-              },
-            ))));
+        home: BlocProvider<ColorBloc>(
+            builder: (context) => ColorBloc(), child: MainPage()));
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ColorBloc bloc = BlocProvider.of<ColorBloc>(context);
+    return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              bloc.dispatch(ColorEvent.to_amber);
+            },
+            backgroundColor: Colors.amber,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              bloc.dispatch(ColorEvent.to_light_blue);
+            },
+            backgroundColor: Colors.lightBlue,
+          )
+        ],
+      ),
+      appBar: AppBar(
+        title: Text("BLoC dengan flutter bloc"),
+      ),
+      body: Center(
+        child: BlocBuilder<ColorBloc, Color>(
+          builder: (context, currentColor) => AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              width: 100,
+              height: 100,
+              color: currentColor),
+        ),
+      ),
+    );
   }
 }
